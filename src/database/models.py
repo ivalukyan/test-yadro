129fb1be8986d04f10ab7586de65d3fcb022c159
ivanlukanov <ivalukyanov1@gmail.com>
2025-05-05 14:06:28 +0300

    Upd: add models

--- src/database/models.py
+++ src/database/models.py
@@ -4,8 +4,20 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
-class User(Base):
+class adjancency_graphs(Base):
     __tablename__ = 'adjacency_graphs'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nodes = Column()
+
+
+class nodes_graphs(Base):
+    id = Column(Integer, nullable=False, autoincrement=True)
+    graph_id = Column(Integer, ForeignKey("adjacency_graphs.id"), nullable=False)
+    name = Column(String, nullable=False)
+
+
+class edges_graphs(Base):
+    id = Column(Integer, nullable=False, autoincrement=True)
+    graph_id = Column(Integer, ForeignKey("adjacency_graphs.id"), nullable=False)
     
