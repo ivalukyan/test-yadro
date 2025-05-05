ab38777d467d56c145c6b3ecfeb827d5be233cc4
ivanlukanov <ivalukyanov1@gmail.com>
2025-05-05 18:20:05 +0300

    Upd: models database

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, BigInteger, Float
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
